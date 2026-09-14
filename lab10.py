28e04c8e723a96586d45c9e14b61f403a1993bc4
noura <nourafaisal22i@gmail.com>
2026-09-14 18:56:01 +0300

    lab10

---
+++ lab10.py
@@ -0,0 +1,12 @@
+def additoin(x, y):
+    x = 10
+    y = 20
+
+
+    try:
+       print("Addition:", x + b)
+       print("success")
+    except NameError:
+     print("variables are not defined")
+
+additoin(10, 20)
